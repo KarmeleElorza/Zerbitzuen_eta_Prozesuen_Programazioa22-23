f54a98a403c78afed65d19b54ef4818e4fa14940
Karmele Elorza <elorza.karmele@uni.eus>
2022-12-01 12:29:08 +0100

    azterketako ariketa ia bukatuta

--- Ariketak/AzterketaKarmele.c
+++ Ariketak/AzterketaKarmele.c
@@ -17,12 +17,13 @@ void main()
     float diruaMurriztu = 0;
     float erabiltzaileakAteratakoa = 0;
     float erabiltzaileakSartutakoa = 0;
-    
-    int fd[2], fd1[2], fd2[2];
+   
+    int fd[2], fd1[2], fd2[2], fd3[2];
     pid_t pid, pid2;
     pipe(fd);
     pipe(fd1);
     pipe(fd2);
+    pipe(fd3);
     pid = fork();
     float ateratotal = 0, sartutotal = 0;
     if (pid == -1)
@@ -36,15 +37,13 @@ void main()
             while (1)
             {
                 //SEMEAK JASO
-                close(fd[1]);//sarrera itxi
-                close(fd2[1]);//sarrera itxi
-                close(fd2[0]);//sarrera itxi
-
-                read(fd[0], erabiltzaileakAteratakoa, sizeof (erabiltzaileakAteratakoa)); //pipea irakurri
+                close(fd3[1]);//sarrera itxi
+                wait(NULL);
+                read(fd3[0], &erabiltzaileakAteratakoa, sizeof (erabiltzaileakAteratakoa)); //pipea irakurri
                 ateratotal = ateratotal + erabiltzaileakAteratakoa;
                 //SEMEAK BIDALI
-                close (fd1[0]);//pipea irakurtzeko aukera itxi
-                write(fd1[1], ateratotal, sizeof(ateratotal));//pipean idatzi
+                close(fd1[0]);//pipea irakurtzeko aukera itxi
+                write(fd1[1], &ateratotal, sizeof(ateratotal));//pipean idatzi
             }
         }
         else // Aita
@@ -62,14 +61,12 @@ void main()
                     {
                         //SEMEAK JASO
                         close(fd[1]);//sarrera itxi
-                        close(fd1[1]);//sarrera itxi
-                        close(fd1[0]);//sarrera itxi
-
-                        read(fd[0], erabiltzaileakSartutakoa, sizeof (erabiltzaileakSartutakoa)); //pipea irakurri
+                        wait(NULL);
+                        read(fd[0], &erabiltzaileakSartutakoa, sizeof (erabiltzaileakSartutakoa)); //pipea irakurri
                         sartutotal = sartutotal + erabiltzaileakSartutakoa;
                         //SEMEAK BIDALI
                         close (fd2[0]);//pipea irakurtzeko aukera itxi
-                        write(fd2[1], sartutotal, sizeof(sartutotal));//pipean idatzi
+                        write(fd2[1], &sartutotal, sizeof(sartutotal));//pipean idatzi
                     }
                 }
                 else // Aita
@@ -93,13 +90,8 @@ void main()
                             {
                                 printf("Kontu Korrontean ez dago hainbeste dirurik...\n");
                             }
-                            close(fd2[1]);//sarrera itxi
-                            close(fd2[0]);//sarrera itxi
-                            close(fd1[1]);//sarrera itxi
-                            close(fd1[0]);//sarrera itxi
-
-                            close(fd[0]);
-                            write(fd[1], diruaMurriztu, sizeof(diruaMurriztu)); // pipean idazten...
+                            close(fd3[0]);
+                            write(fd3[1], &diruaMurriztu, sizeof(diruaMurriztu)); // pipean idazten...
                             //wait(NULL);
                             break;
 
@@ -108,22 +100,19 @@ void main()
                             scanf("%f", &diruaGehitu);
                             hasierakoDirua = hasierakoDirua + diruaGehitu;
                             printf("Kontu Korronteko Saldoa: %f\n", hasierakoDirua);
-                            close(fd2[1]);//sarrera itxi
-                            close(fd2[0]);//sarrera itxi
-                            close(fd1[1]);//sarrera itxi
-                            close(fd1[0]);//sarrera itxi
                             close(fd[0]);
-                            write(fd[1], diruaGehitu, sizeof(diruaGehitu)); // pipean idazten...
+                            write(fd[1], &diruaGehitu, sizeof(diruaGehitu)); // pipean idazten...
                             //wait(NULL);
                             break;
                         case 3:
-                            read(fd2[0], sartutotal, sizeof(sartutotal));//pipea irakurri       
-                            read(fd1[0], ateratotal, sizeof(ateratotal));//pipea irakurri
-                            read(fd[0], hasierakoDirua, sizeof(hasierakoDirua));//pipea irakurri
-                            
+                            //close(fd[1]); ///sarrera itxi
+                            //close(fd3[1]); ///sarrera itxi
+                            //close(fd[0]); ///sarrera itxi
                             close(fd2[1]);//sarrera itxi
+                            read(fd2[0], &sartutotal, sizeof(sartutotal));//pipea irakurri 
                             close(fd1[1]);//sarrera itxi
-                            close(fd[1]); ///sarrera itxi
+                            read(fd1[0], &ateratotal, sizeof(ateratotal));//pipea irakurri
+                            //read(fd[0], &hasierakoDirua, sizeof(hasierakoDirua));//pipea irakurri
 
                             printf("Gaur egungo Kontu Korronteko saldoa honako hau da : %f\n", hasierakoDirua);
                             printf("%f ateraldi egin dira guztira\n", ateratotal);
